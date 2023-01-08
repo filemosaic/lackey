b12bae1427fe6ccab6bbf4bb73c068fccca52c46
DogAllmighty <96669089+DogAllmighty@users.noreply.github.com>
2023-01-08 16:02:23 -0700

    Initial project init (#1)

---
+++ src/lackey/lackey/CommandHandlers/SandboxCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lackey.CommandHandlers
+{
+    internal static class SandboxCommandHandler
+    {
+        internal static void Handle()
+        {
+            Console.WriteLine("Sandbox command has been handled!");
+        }
+    }
+}
